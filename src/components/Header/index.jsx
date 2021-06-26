74ef71281dd7fcb1835f1ae23040e99b7c82dcef
Asia Alnahi <asia.alius@gmail.com>
2021-06-25 21:00:59 -0700

    Creating the header and Add logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,16 @@ import "./header.scss";
 export default function Header() {
     return (
         <div className="header">
-           
+           <div className="wrapper">
+               <div className="left">
+                    <div class="logo">
+                        <a href="#intro">aa</a>
+                    </div>
+               </div>
+               <div className="right">
+
+               </div>
+           </div>
             
         </div>
     )
